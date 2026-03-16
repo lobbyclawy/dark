67ac16e29fb8894b753920e68b72e14a954c92e2
Lobby <lobby.clawy@gmail.com>
2026-03-16 14:13:26 -0500

    🐛 Fix duplicate offchain_tx_repo binding (unused variable, #173)

--- src/main.rs
+++ src/main.rs
@@ -110,10 +110,7 @@ async fn main() -> Result<()> {
 
     let round_repo = Arc::new(arkd_db::SqliteRoundRepository::new(sqlite_pool.clone()));
     let vtxo_repo = Arc::new(arkd_db::SqliteVtxoRepository::new(sqlite_pool.clone()));
-    let offchain_tx_repo = Arc::new(arkd_db::SqliteOffchainTxRepository::new(
-        sqlite_pool.clone(),
-    ));
-    let offchain_tx_repo = Arc::new(arkd_db::repos::SqliteOffchainTxRepository::new(sqlite_pool));
+    let offchain_tx_repo = Arc::new(arkd_db::SqliteOffchainTxRepository::new(sqlite_pool.clone()));
 
     // --- Blockchain scanner ---
     let scanner: Arc<dyn arkd_core::ports::BlockchainScanner> =
